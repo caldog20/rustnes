49ae7698a8af6083413c6a2381d86d9b000d916f
Caleb <caldog20@gmail.com>
2021-01-22 19:43:56 -0600

    LDA function new model for address modes

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,13 +1,20 @@
-use crate::cpu::CPU;
+use crate::cpu::*;
 
 
-pub fn lda(cpu: &mut CPU, value: u8) {
-    cpu.registers.a = value;
-    cpu.zero_neg_flags(cpu.registers.a);
-    cpu.registers.pc += 1;
-}
+// pub fn lda(cpu: &mut CPU, value: u8) {
+//     cpu.registers.a = value;
+//     cpu.zero_neg_flags(cpu.registers.a);
+//     cpu.registers.pc += 1;
+// }
 pub fn tax(cpu: &mut CPU) {
     cpu.registers.x = cpu.registers.a;
     cpu.zero_neg_flags(cpu.registers.x);
     cpu.registers.pc += 1;
+}
+pub fn lda(cpu: &mut CPU, mode: &Modes) {
+    let addr = cpu.get_address(&mode);
+    let value = cpu.read_mem(addr);
+    cpu.registers.a = value;
+    cpu.zero_neg_flags(cpu.registers.a);
+    println!("LDA ADDR: {:#04X} VAL: {:#04X}", addr, value);
 }
